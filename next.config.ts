1f966303115baeec2f4a6ed1b10ab6910e5f9339
gleicesantos22 <gleicesantospeixoto@gmail.com>
2025-03-05 23:33:55 +0400

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["fundly.com", "images.fundly.com", "images.gofundme.com", "images.gofundme.com", "static.vakinha.com"],
-  },
-  // ... other configurations ...
-};
-
-module.exports = nextConfig;
